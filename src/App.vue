0a7dfcbe3c6e8fe37a8c317e500d6fa9175aa2bf
Omniamutanturnihilinterit <92959023+Omniamutanturnihilinterit@users.noreply.github.com>
2023-01-29 00:55:46 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,316 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const questions = ref([
+  {
+	question: 'Bahasa pemrograman Android yakni?',
+	answer: 0,
+	options: [
+		'Kotlin, Flutter, React Native',
+		'React Js, Vue JS, Svelte Js',
+		'PHP, Laravel, CI'
+	],
+	selected: null
+  },
+  {
+	question: 'Aplikasi untuk mengedit HTML yakni?',
+	answer: 2,
+	options: [
+		'Turbo Pascal',
+		'Intellij IDEA',
+		'VS Code'
+	],
+	selected: null
+  },
+  {
+	question: 'Berikut ini merupakan framework Javascript, kecuali?',
+	answer: 1,
+	options: [
+		'AngularJS',
+		'Java',
+		'ReactJS'
+	],
+	selected: null
+  },
+  {
+	question: 'Sebuah web untuk menyimpan sebuah kode/project adalah?',
+	answer: 0,
+	options: [
+		'Benar Semua',
+		'Gitlab',
+		'Github'
+	],
+	selected: null
+  },
+  {
+	question: 'Untuk menjalankan project Codeigniter, lakukan perintah?',
+	answer: 2,
+	options: [
+		'php spark migrate:create news',
+		'flutter pub get',
+		'php spark serve'
+	],
+	selected: null
+  },
+  {
+	question: 'Java merupakan bahasa pemrograman yang dapat dijalankan di berbagai komputer termasuk telepon genggam. Bahasa ini awalnya dibuat oleh?',
+	answer: 1,
+	options: [
+		'Rasmus Lerdorf',
+		'James Gosling',
+		'Bill Gates'
+	],
+	selected: null
+  },
+  {
+	question: 'CSS salah satu styling language (bahasa desain), bagian dari markup language yang dapat "mewarnai" atau mendesain suatu halaman website. CSS sendiri singkatan dari?',
+	answer: 1,
+	options: [
+		'Extensible Markup Language',
+		'Cascading Style Sheet',
+		'Hypertext Prepocessor'
+	],
+	selected: null
+  },
+  {
+	question: 'Di bawah ini merupakan tag javascript yang dapat disisipkan pada dokumen HTML?',
+	answer: 0,
+	options: [
+		'<script>',
+		'<jquery>',
+		'<js>'
+	],
+	selected: null
+  },
+  {
+	question: 'Status dalam bahasa pemrograman biasanya disimpan sebagai?',
+	answer: 0,
+	options: [
+		'Variabel',
+		'Method',
+		'Class'
+	],
+	selected: null
+  },
+  {
+	question: 'Dibawah ini manakah yang merupakan keyword JAVA?',
+	answer: 2,
+	options: [
+		'Abstrack',
+		'Boolean',
+		'Benar semua'
+	],
+	selected: null
+  }
+])
+
+const quizCompleted = ref(false)
+const currentQuestion = ref(0)
+const score = computed(() => {
+	let value = 0
+	questions.value.map(q => {
+		if (q.selected != null && q.answer == q.selected) {
+			console.log('correct');
+			value++
+		}
+	})
+	return value
+})
+
+const getCurrentQuestion = computed(() => {
+	let question = questions.value[currentQuestion.value]
+	question.index = currentQuestion.value
+	return question
+})
+
+const SetAnswer = (e) => {
+	questions.value[currentQuestion.value].selected = e.target.value
+	e.target.value = null
+}
+
+const NextQuestion = () => {
+	if (currentQuestion.value < questions.value.length - 1) {
+		currentQuestion.value++
+		return
+	}
+	
+	quizCompleted.value = true
+}
+</script>
+
+<template>
+	<main class="app">
+		<h1>Quiz App</h1>
+		<h1>Quiz Programming and Coding</h1>
+		
+		<section class="quiz" v-if="!quizCompleted">
+			<div class="quiz-info">
+				<span class="question">{{ getCurrentQuestion.question }}</span>
+				<span class="score">Score {{ score }}/{{ questions.length }}</span>
+			</div>
+			
+			<div class="options">
+				<label 
+					v-for="(option, index) in getCurrentQuestion.options" 
+					:for="'option' + index" 
+					:class="`option ${
+						getCurrentQuestion.selected == index 
+							? index == getCurrentQuestion.answer 
+								? 'correct' 
+								: 'wrong'
+							: ''
+					} ${
+						getCurrentQuestion.selected != null &&
+						index != getCurrentQuestion.selected
+							? 'disabled'
+							: ''
+					}`">
+					<input 
+						type="radio" 
+						:id="'option' + index" 
+						:name="getCurrentQuestion.index" 
+						:value="index" 
+						v-model="getCurrentQuestion.selected" 
+						:disabled="getCurrentQuestion.selected"
+						@change="SetAnswer" 
+					/>
+					<span>{{ option }}</span>
+				</label>
+			</div>
+			
+			<button 
+				@click="NextQuestion" 
+				:disabled="!getCurrentQuestion.selected">
+				{{ 
+					getCurrentQuestion.index == questions.length - 1 
+						? 'Finish' 
+						: getCurrentQuestion.selected == null
+							? 'Select an option'
+							: 'Next question'
+				}}
+			</button>
+		</section>
+
+		<section v-else>
+			<h2>You have finished the quiz!</h2>
+			<p>Your score is {{ score }}/{{ questions.length }}</p>
+		</section>
+	</main>
+</template>
+
+<style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Montserrat', sans-serif;
+}
+
+body {
+	background-color: #271c36;
+	color: #FFF;
+}
+
+.app {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 2rem;
+	height: 100vh;
+}
+
+h1 {
+	font-size: 2rem;
+	margin-bottom: 2rem;
+}
+
+.quiz {
+	background-color: #382a4b;
+	padding: 1rem;
+	width: 100%;
+	max-width: 640px;
+}
+
+.quiz-info {
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 1rem;
+}
+
+.quiz-info .question {
+	color: #8F8F8F;
+	font-size: 1.25rem;
+}
+
+.quiz-info.score {
+	color: #FFF;
+	font-size: 1.25rem;
+}
+
+.options {
+	margin-bottom: 1rem;
+}
+
+.option {
+	padding: 1rem;
+	display: block;
+	background-color: #271c36;
+	margin-bottom: 0.5rem;
+	border-radius: 0.5rem;
+	cursor: pointer;
+}
+
+.option:hover {
+	background-color: #2d213f;
+}
+
+.option.correct {
+	background-color: #2cce7d;
+}
+
+.option.wrong {
+	background-color: #ff5a5f;
+}
+
+.option:last-of-type {
+	margin-bottom: 0;
+}
+
+.option.disabled {
+	opacity: 0.5;
+}
+
+.option input {
+	display: none;
+}
+
+button {
+	appearance: none;
+	outline: none;
+	border: none;
+	cursor: pointer;
+	padding: 0.5rem 1rem;
+	background-color: #2cce7d;
+	color: #2d213f;
+	font-weight: 700;
+	text-transform: uppercase;
+	font-size: 1.2rem;
+	border-radius: 0.5rem;
+}
+
+button:disabled {
+	opacity: 0.5;
+}
+
+h2 {
+	font-size: 2rem;
+	margin-bottom: 2rem;
+	text-align: center;
+}
+
+p {
+	color: #8F8F8F;
+	font-size: 1.5rem;
+	text-align: center;
+}
+</style>
